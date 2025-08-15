601cf109e8eb91225e7ec4eb3e728a71f3bd4e98
ken <block24block@gmail.com>
2025-08-15 14:40:58 +0800

    prompt style

--- src/azx/prompt.py
+++ src/azx/prompt.py
@@ -1,5 +1,13 @@
 from prompt_toolkit import PromptSession
+from prompt_toolkit.styles import Style
+
+style = Style.from_dict(
+    {
+        "prompt": "ansigreen",  # Blue prompt text
+        "": "ansigreen",  # Green input text
+    }
+)
 
 
 def session() -> PromptSession:
-    return PromptSession(">>> ")
+    return PromptSession(">>> ", style=style)
