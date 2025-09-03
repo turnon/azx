4a6dee5af31f4a782d9e16d52af14c6eae8a7899
ken <block24block@gmail.com>
2025-09-03 09:19:21 +0800

    no search_wiki any more

--- src/azx/tools.py
+++ src/azx/tools.py
@@ -3,29 +3,10 @@ import json
 import os
 import re
 import shutil
-import tempfile
 
-import requests
 from markitdown import MarkItDown
 
 definitions = [
-    {
-        "type": "function",
-        "function": {
-            "name": "search_wiki",
-            "description": "Search on wikipedia to get detail info",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "keyword": {
-                        "type": "string",
-                        "description": "keyword to search wikipedia",
-                    },
-                },
-                "required": ["keyword"],
-            },
-        },
-    },
     {
         "type": "function",
         "function": {
@@ -252,50 +233,6 @@ class Tools:
             return {"status": "success", "data": None, "err": None, "proceed": None}
         except Exception as e:
             return {"status": "error", "data": None, "err": str(e), "proceed": None}
-
-    @staticmethod
-    def search_wiki(keyword) -> dict:
-        md_pages = []
-        md = MarkItDown()
-        url = "https://en.wikipedia.org/w/api.php"
-        headers = {"User-Agent": "MyApp/1.0 (your.email@example.com)"}
-
-        keyword_params = {
-            "action": "opensearch",
-            "format": "json",
-            "search": keyword,
-            "limit": 10,
-        }
-        keyword_resp = requests.get(url, params=keyword_params, headers=headers)
-
-        for syn in keyword_resp.json()[1]:
-            title_args = {
-                "action": "query",
-                "format": "json",
-                "titles": syn,
-                "prop": "extracts",
-            }
-            title_resp = requests.get(url, params=title_args, headers=headers)
-            data = title_resp.json()
-            pages = data["query"]["pages"]
-            for page in pages.values():
-                if "extract" in page:
-                    text = f"# {syn}\n\n"
-                    with tempfile.NamedTemporaryFile(
-                        mode="w", suffix=".html", delete=False, encoding="utf-8"
-                    ) as temp_file:
-                        temp_file.write(page["extract"])
-                        temp_file_path = temp_file.name
-                    md.convert(temp_file_path).text_content
-                    text += md.convert(temp_file_path).text_content
-                    md_pages.append(text)
-
-        return {
-            "status": "success",
-            "data": "\n\n---\n\n".join(md_pages),
-            "err": None,
-            "proceed": None,
-        }
 
     @staticmethod
     def grep_files(path, regexp) -> dict:
